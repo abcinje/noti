d2b894b62695e2d47609a685de210f1cd603950c
Injae Kang <abcinje@gmail.com>
2022-12-24 14:04:03 +0900

    Fix minor

--- main.rs
+++ main.rs
@@ -1,11 +1,10 @@
 use std::env;
 use std::process;
-use std::process::Command;
 use std::thread;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    if args.len() == 1 {
         eprintln!("Usage: {} <cmd> [args]", args[0]);
         process::exit(1);
     }
@@ -13,10 +12,10 @@ fn main() {
     let cmd = &args[1];
     let args = &args[2..];
 
-    let mut process = Command::new(cmd)
-                              .args(args)
-                              .spawn()
-                              .expect("Failed to execute command");
+    let mut process = process::Command::new(cmd)
+                                       .args(args)
+                                       .spawn()
+                                       .expect("Failed to execute command");
     println!("Process started with pid: {}", process.id());
 
     loop {
