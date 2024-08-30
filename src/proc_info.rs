4993be886f5a7886ffd944d3413d708d55b73173
Injae Kang <abcinje@gmail.com>
2024-08-31 01:31:43 +0900

    Fix minor

--- src/proc_info.rs
+++ src/proc_info.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ProcInfo {
-	pub command: String,
-	pub pid: u32,
-	pub status: String,
+    pub command: String,
+    pub pid: u32,
+    pub status: String,
 }
