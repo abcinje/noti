a7f7e564aabf73b5abab11ea6d484422595a55bf
Injae Kang <abcinje@gmail.com>
2024-08-30 10:54:30 +0900

    Fix minor

--- src/noti.rs
+++ src/noti.rs
@@ -1,35 +1,15 @@
 use std::io::{self, BufWriter, Write};
 use std::net::TcpStream;
-use std::process::{Child, Command, ExitStatus};
-use std::{env, process, thread};
+use std::process::{Child, Command};
+use std::{env, process};
 
 mod proc_info;
 use proc_info::ProcInfo;
 
-fn launch(args: &[String]) -> Child {
+fn launch(args: &[String]) -> io::Result<Child> {
     let cmd = &args[0];
     let args = &args[1..];
-
-    Command::new(cmd)
-        .args(args)
-        .spawn()
-        .expect("Failed to execute command")
-}
-
-fn wait(mut proc: Child) -> ExitStatus {
-    loop {
-        match proc.try_wait() {
-            Ok(Some(status)) => {
-                println!("Process exited with: {status}");
-                break status;
-            }
-            Err(error) => {
-                eprintln!("Error attempting to wait: {error}");
-                process::exit(1);
-            }
-            _ => thread::yield_now(),
-        };
-    }
+    Command::new(cmd).args(args).spawn()
 }
 
 fn main() -> io::Result<()> {
@@ -39,16 +19,16 @@ fn main() -> io::Result<()> {
         process::exit(1);
     }
 
-    let args = &args[1..];
-    let proc = launch(&args);
+    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
 
+    let args = &args[1..];
+    let mut proc = launch(&args)?;
     let proc_info = ProcInfo {
         command: args.join(" "),
         pid: proc.id(),
-        status: wait(proc).to_string(),
+        status: proc.wait()?.to_string(),
     };
 
-    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
     let mut writer = BufWriter::new(&mut stream);
     let content = serde_json::to_string(&proc_info)?;
     writer.write(content.as_bytes())?;
